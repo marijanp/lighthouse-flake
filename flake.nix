3b3cb409eaec872788945be123265d80ddb65b52
Marijan Petričević <marijan.petricevic94@gmail.com>
2023-04-11 15:48:58 +0200

    add postBuild override

--- flake.nix
+++ flake.nix
@@ -5,12 +5,21 @@
     lighthouse-src.flake = false;
   };
   outputs = { self, dream2nix, ... }:
+    let
+      pkgs = dream2nix.inputs.nixpkgs.legacyPackages.x86_64-linux;
+    in
     {
       inherit (dream2nix.lib.makeFlakeOutputs {
         systems = [ "x86_64-linux" ];
         source = self.inputs.lighthouse-src;
         projects = ./projects.toml;
         config.projectRoot = ./.;
+        packageOverrides = {
+          "lighthouse".updated.overrideAttrs = old: {
+            nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.yarn ];
+            postBuild = "npm run build-all";
+          };
+        };
       }) packages;
     };
 }
