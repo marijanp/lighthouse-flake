5ec0ac8800282e384acb3b53850bd932c60d38cf
Marijan Petričević <marijan.petricevic94@gmail.com>
2025-03-11 03:46:30 +0100

    flake-module: use pkgs.lighthouse, h2o to serve the dist

--- flake-module.nix
+++ flake-module.nix
@@ -69,42 +69,67 @@ in {
   config = {
     perSystem = { config, pkgs, ... }:
       let
-        server-port = 8080;
         checks = lib.mapAttrs (name: test:
           pkgs.nixosTest {
             inherit name;
             nodes = {
-              server = { pkgs, ... }: {
+              server = { pkgs, config, ... }: {
                 environment.variables.CHROME_PATH =
                   lib.getExe pkgs.ungoogled-chromium;
-                systemd.services.lighthouse-test-server = {
-                  wantedBy = [ "multi-user.target" ];
-                  after = [ "network.target" ];
-                  description = "lighthouse-test-server";
-                  serviceConfig = {
-                    DynamicUser = true;
-                    ExecStart = "${
-                        lib.getExe pkgs.simple-http-server
-                      } -c=js,css,svg,html -i -p ${
-                        builtins.toString server-port
-                      } -- ${test.dist}";
+
+                services.h2o = {
+                  enable = true;
+                  settings = {
+                     compress = "ON";
+                     http2-reprioritize-blocking-assets = "ON";
+                     ssl-offload = "kernel";
                   };
+                  hosts."acme.test" = {
+                    tls = {
+                      recommendations = "modern";
+                      policy = "force";
+                      identity = [
+                        {
+                          key-file = pkgs.path + "/nixos/tests/common/acme/server/acme.test.key.pem";
+                          certificate-file = pkgs.path + "/nixos/tests/common/acme/server/acme.test.cert.pem";
+                        }
+                      ];
+                    };
+                    settings = {
+                      paths."/" = {
+                        "file.dir" = test.dist;
+                      };
+                    };
+                   };
                 };
+
+                networking = {
+                  firewall.allowedTCPPorts = [ config.services.h2o.defaultTLSListenPort ];
+                  extraHosts = ''
+                    127.0.0.1 acme.test
+                  '';
+                };
+                # Required to make TLS work
+                security.pki.certificates = [
+                  (builtins.readFile (pkgs.path + "/nixos/tests/common/acme/server/ca.cert.pem"))
+                ];
               };
             };
 
-            testScript = ''
+            testScript = { nodes, ... }: ''
               import json
               import os
 
               start_all()
-              server.wait_for_open_port(${builtins.toString server-port})
+              server.wait_for_open_port(${builtins.toString nodes.server.services.h2o.defaultTLSListenPort})
+              server.wait_for_unit("h2o.service")
 
               report_path = "/tmp/lighthouse-report.json"
-              server.succeed("CI=1 ${config.lighthouse.package}/bin/lighthouse http://localhost:${
-                builtins.toString server-port
-              } --output json --output-path {} --only-categories accessibility,best-practices,performance,seo --skip-audits valid-source-maps --chrome-flags=\"--headless --no-sandbox\"".format(report_path))
+
+              server.succeed("CI=1 ${lib.getExe config.lighthouse.package} https://acme.test --output json --output-path {} --only-categories accessibility,best-practices,performance,seo --skip-audits valid-source-maps --chrome-flags=\"--headless --no-sandbox\"".format(report_path))
+
               server.copy_from_vm(report_path)
+
               with open("{}/lighthouse-report.json".format(os.environ["out"]), "r") as f:
                 report = json.load(f)
                 categories = report["categories"]
@@ -114,28 +139,28 @@ in {
                   toString test.categories.performance
                 }, "performance score should be at least ${
                   toString test.categories.performance
-                }, but was {}".format(performance_score)
+                }, but it was {}".format(performance_score)
 
                 accessibility_score = categories["accessibility"]["score"]
                 assert accessibility_score >= ${
                   toString test.categories.accessibility
                 }, "accessibility score should be at least ${
                   toString test.categories.accessibility
-                }, but was {}".format(accessibility_score)
+                }, but it was {}".format(accessibility_score)
 
                 seo_score = categories["seo"]["score"]
                 assert seo_score >= ${
                   toString test.categories.seo
                 }, "seo score should be at least ${
                   toString test.categories.seo
-                }%, but it was {}".format(seo_score)
+                }, but it was {}".format(seo_score)
 
                 best_practices_score = categories["best-practices"]["score"]
                 assert best_practices_score >= ${
                   toString test.categories.bestPractices
                 }, "best-practices score should be at least ${
                   toString (test.categories.bestPractices)
-                }"
+                }, but it was {}".format(best_practices_score)
             '';
           }) config.lighthouse.tests;
       in { inherit checks; };
